6d56d54e4fd7773c37730aab4baf3fb2b00014c1
ryanenoch <105472843+ryanenoch@users.noreply.github.com>
2023-06-11 21:47:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return jsonify({"Welcome to the Test Flask app"})
+def homepage():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
